4a70a743c13041a221751611eaff99b9b7cc5be3
Mats Johannesson <mats@air.local>
2022-10-17 18:08:13 +0200

    Added javaserver

---
+++ javaserver/src/test/java/se/ryz/cannon/CannonApplicationTests.java
@@ -0,0 +1,13 @@
+package se.ryz.cannon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CannonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
