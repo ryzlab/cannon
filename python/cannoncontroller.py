9c80ba9200d6be0273a336f2660aa3b7373813ea
Mats Johannesson <ryzlab@ryz.se>
2022-10-18 12:50:28 +0200

    Fixed some

--- python/cannoncontroller.py
+++ python/cannoncontroller.py
@@ -2,8 +2,7 @@ import re
 import time
 
 def validateCommandIsKeyword(cmd):
-	if cmd not in ["up", "down", "left", "right", "stop", "fire"]:
-		print("ERROR: Command is not in up, down, left, right, stop, fire")
+	return cmd in ["up", "down", "left", "right", "stop", "fire"]
 
 def performCommand(cmd, delay):
 	print("Sending command '" + cmd + "'")
@@ -14,25 +13,28 @@ def performCommand(cmd, delay):
 	print("OK")
 
 cmdPattern = re.compile("^([a-z]+)( ([0-9]+))?$")
+print(">> Cannon Controller. Use commands up, down, left, right, stop and fire with an optional duration")
 while True:
 	cmdLine = input()
 	match = cmdPattern.match(cmdLine)
 	if match:
 		cmd = match.group(1)
-		validateCommandIsKeyword(cmd)
-		delayStr = match.group(3)
-		if delayStr:
-			delay = int(delayStr)
-			if cmd == "fire" or cmd == "stop":
-				print("ERROR: 'fire' and 'stop' does not take arguments")
-			else:
-				if delay > 10000:
-					print("ERROR: Delay must not be greater than 10000")
-				else:
-					performCommand(cmd, delay)
-					#print("cmd: '" + cmd + "', arg: '" + arg + "'")
+		if not validateCommandIsKeyword(cmd):
+			print("ERROR: Command is not in up, down, left, right, stop, fire")
 		else:
-			performCommand(cmd, None)
+			durationStr = match.group(3)
+			if durationStr:
+				duration = int(durationStr)
+				if cmd == "fire" or cmd == "stop":
+					print("ERROR: 'fire' and 'stop' does not take arguments")
+				else:
+					if duration > 10000:
+						print("ERROR: Delay must not be greater than 10000")
+					else:
+						performCommand(cmd, duration)
+						#print("cmd: '" + cmd + "', arg: '" + arg + "'")
+			else:
+				performCommand(cmd, None)
 			#print("cmd: '" + cmd + "'")
 	else:
 		print("ERROR: Invalid command")
