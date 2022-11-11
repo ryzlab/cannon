574701c673fb0f2e719d4d34242fd2bd457d81ff
Mats Johannesson <mats@Matss-MacBook-Pro.local>
2022-11-11 11:16:47 +0100

    Initial revision

---
+++ nodemcu/mqtt/src/environment.h
@@ -0,0 +1,8 @@
+#ifndef __ENVIRONMENT_H__
+#define __ENVIRONMENT_H__
+
+#define WIFI_SSID "SSID goes here"
+#define WIFI_PASSWORD "WiFi password here"
+#define MQTT_SERVER "IP Address of MQTT server goes here"
+
+#endif // __ENVIRONMENT_H__
